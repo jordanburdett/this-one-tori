442270352f4c5dd3d6c48b79eb48b1bb3237b7cd
Jordan <jordan@burdett.us>
2019-11-12 14:06:38 -0700

    test

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const path = require('path')
+var fs = require('fs')
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', function (req, res) {
+
+    fs.readdir('views/pages', function(err, f) {
+      if (err) console.log(process.cwd());
+      var files = f;
+      f.forEach(function(file) {
+        console.log(file);
+      })
+
+      console.log(files);
+      
+      res.render('pages/index', {
+        files: files
+      })
+    })
+    
+  })
+  .get('/math', (req, res) => res.render('pages/math'))
+  .get('/db', (req, res) => res.render('pages/db'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
