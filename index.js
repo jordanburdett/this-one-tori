1a145f0ecd429bb7eafd00d6c39aec691323f65b
Jordan <jordan@burdett.us>
2019-11-19 14:26:10 -0700

    db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 var fs = require('fs')
 const PORT = process.env.PORT || 5000
-const connectionString = process.env.DATABASE_URL || "postgres://rwomixvqbgubak:8b23aeaeda698af90a27e3a3d059edc4b3c7325c125705339b722caf7ba15e7d@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d5nbldkuhdg9ue?ssl=true"
+const connectionString = process.env.DATABASE_URL || "postgres://rwomixvqbgubak:8b23aeaeda698af90a27e3a3d059edc4b3c7325c125705339b722caf7ba15e7d@ec2-54-83-61-142.compute-1.amazonaws.com:5432/d5nbldkuhdg9ue?ssl=true";
 
 express()
   .use(express.urlencoded())
@@ -10,7 +10,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', function (req, res) {
-
     fs.readdir('views/pages', function (err, f) {
       if (err) console.log(process.cwd());
       var files = f;
@@ -40,38 +39,37 @@ express()
       // Log this to the console for debugging purposes.
       console.log("Back from DB with result:");
       console.log(result.rows);
-    })
-      .get('/math', (req, res) => res.render('pages/math'))
-      .get('/db', (req, res) => res.render('pages/db'))
-      .post('/domath', function (req, res, next) {
-        let num1 = Number(req.body.num1);
-        let num2 = Number(req.body.num2);
-        let oper = req.body.operator;
+    }
+  )})
+  .get('/math', (req, res) => res.render('pages/math'))
+  .get('/db', (req, res) => res.render('pages/db'))
+  .post('/domath', function (req, res, next) {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let oper = req.body.operator;
 
-        let answer = undefined;
-        switch (oper) {
-          case '+':
-            answer = num1 + num2;
-            break;
-          case '-':
-            answer = num1 - num2;
-            break;
-          case '*':
-            answer = num1 * num2;
-            break;
-          case '/':
-            answer = num1 / num2;
-            break;
-          default:
-            answer = "ERROR!";
-        }
+    let answer = undefined;
+    switch (oper) {
+      case '+':
+        answer = num1 + num2;
+        break;
+      case '-':
+        answer = num1 - num2;
+        break;
+      case '*':
+        answer = num1 * num2;
+        break;
+      case '/':
+        answer = num1 / num2;
+        break;
+      default:
+        answer = "ERROR!";
+    }
 
-        console.log(answer);
+    console.log(answer);
 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write("<div class='display-4'>The answer is " + answer.toString() + "</div>");
-        res.end();
-      })
-
-      .listen(PORT, () => console.log(`Listening on ${PORT}`))
-
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("<div class='display-4'>The answer is " + answer.toString() + "</div>");
+    res.end();
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
